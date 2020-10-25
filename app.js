71dc7150f6adff567d789baa52313b898a7b7a35
developerfauzan <developerfauzan@gmail.com>
2020-10-26 06:56:14 +0700

    feat(server): Add CORS and Update Port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const index = require('./routes/index');
 const tripleDes = require('./routes/3des');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/3des', tripleDes);
